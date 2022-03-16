217a968181a231dd8d6cec4e1466d73540ebcb2d
Adrian Schneider <adrian.schneider@rgbi.ch>
2022-03-16 16:52:17 +0100

    Update team-members.php

--- team-members.php
+++ team-members.php
@@ -4,9 +4,9 @@ namespace RGBTeamMembers;
 
 /**
  * Plugin Name:       Team Members
- * Plugin URI:        https://redgreenbird.com
+ * Plugin URI:        https://github.com/redgreenbird-media/wordpress-team-members
  * Description:       This Plugin handles all Team Members. 
- * Version:           0.1
+ * Version:           1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            redgreenbird GmbH
@@ -17,6 +17,7 @@ namespace RGBTeamMembers;
  * Text Domain:       team-members
  * Domain Path:       /languages
  * @copyright         2022 redgreenbird GmbH
+ * GitHub Plugin URI: https://github.com/redgreenbird-media/wordpress-team-members.git
  */
 
 // Define File Path
